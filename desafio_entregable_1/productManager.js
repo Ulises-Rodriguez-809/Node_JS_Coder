8f9e0648bc11a96889949d034ba9ee5f87740b60
Ulises <96356752+Ulises-Rodriguez@users.noreply.github.com>
2023-11-01 16:29:30 -0300

    entrega lista

--- desafio_entregable_1/productManager.js
+++ desafio_entregable_1/productManager.js
@@ -4,9 +4,17 @@ class ProductManager {
     }
 
     addProduct(product){
+
+        const productKeys = Object.keys(product);
+
+        productKeys.forEach(key => {
+            if (key.trim() === "" || key === undefined || key === null) {
+                return `El campo ${key} se ecnuentra vacio, igrese correctamente su valor`;
+            }
+        })
+
         const productoEncontrado = this.products.find(element => element.code === product.code);
 
-        //FALTA COMPROBAR Q NINGUNO DE LOS CAMPOS ES NULL
         if (productoEncontrado === undefined) {
             this.products.push({
                 ...product,
@@ -20,40 +28,86 @@ class ProductManager {
     }
 
     getProductById(idProducto){
-        const productoEncontrado = this.products.find(product => product.id === idProducto);
+        let productoEncontrado = this.products.find(product => product.id === idProducto);
 
-        if (productoEncontrado) {
-            return productoEncontrado
-        } else {
-            console.log(`No se encontro el porducto con el con el ID : ${idProducto}`);
-        }
+        productoEncontrado ? productoEncontrado : productoEncontrado = `No se encontro el porducto con el con el ID : ${idProducto}`;
+
+        return productoEncontrado;
     }
 }
 
 
-//esto ponelo dentro de un array y para agregarlos hacelo con un foreach NO LO DEJES ASI CHOTO
-//HACETE UN REPO EN GITHUB
-const product1 = {
-    title : "Monster Blanco",
-    descripcion : "Bebida energetica sin azucar",
-    price : 450,
-    thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
-    code : 45789517,
-    stock : 15
-}
+//array de productos
+const productosArr = [
+    {
+        title : "Monster Blanco",
+        descripcion : "Bebida energetica sin azucar",
+        price : 450,
+        thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
+        code : 45789517,
+        stock : 15
+    },
+    {
+        title : "Pepitos",
+        descripcion : "Masitas de trigo y chocolate",
+        price : 100,
+        thumbnail : "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/750/456/products/galletitas-pepitos1-997665b2ca5ad112c016255133287229-640-0.jpeg",
+        code : 15978643,
+        stock : 10
+    },
+    {
+        title : "Mismo code",
+        descripcion : "probar q no se agrega si tiene el mismo code",
+        price : 450,
+        thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
+        code : 45789517,
+        stock : 15
+    },
+    {
+        title : "                  ",
+        descripcion : "probar q no se agrega tiene algun key-value sin completar",
+        price : 450,
+        thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
+        code : 345345435347,
+        stock : 15
+    },
+    {
+        title : null,
+        descripcion : "probar q no se agrega si tiene algun key-value sin completar",
+        price : 450,
+        thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
+        code : 487867867817,
+        stock : 15
+    },
+    {
+        title : undefined,
+        descripcion : "probar q no se agrega si tiene algun key-value sin completar",
+        price : 450,
+        thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
+        code : 489089098017,
+        stock : 15
+    }
+];
 
-const product2 = {
-    title : "Pepitos",
-    descripcion : "Masitas de trigo y chocolate",
-    price : 100,
-    thumbnail : "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/750/456/products/galletitas-pepitos1-997665b2ca5ad112c016255133287229-640-0.jpeg",
-    code : 457895173,
-    stock : 10
-}
-
+//intancia de la clase ProductManager
 const productos = new ProductManager();
 
-console.log(productos.getProducts());
-productos.addProduct(product1);
-productos.addProduct(product2);
-console.log(productos.getProducts());
+//fun para añadir productos
+const añadirProductos = (arr,obj)=>{
+    arr.forEach(producto => obj.addProduct(producto));
+}
+
+//fun para obtener la lista de productos
+const obtenerProductos = (obj)=>{
+    console.log(obj.getProducts());
+}
+
+//fun para obtener el producto por su id
+const obtenerProductoPorId = (obj,id)=>{
+    console.log(obj.getProductById(id));
+}
+
+añadirProductos(productosArr,productos);
+obtenerProductos(productos);
+obtenerProductoPorId(productos,1);
+obtenerProductoPorId(productos,156);
