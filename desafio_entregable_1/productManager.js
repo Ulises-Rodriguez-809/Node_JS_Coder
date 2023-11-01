84f1a22692207d1ed78d6f38339a2e098ad67d52
Ulises <96356752+Ulises-Rodriguez@users.noreply.github.com>
2023-11-01 20:11:45 -0300

    modificando los codes de los productos

--- desafio_entregable_1/productManager.js
+++ desafio_entregable_1/productManager.js
@@ -65,7 +65,7 @@ const productosArr = [
         descripcion : "Bebida energetica sin azucar",
         price : 450,
         thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
-        code : 45789517,
+        code : "48aec566",
         stock : 15
     },
     {
@@ -73,7 +73,7 @@ const productosArr = [
         descripcion : "Masitas de trigo y chocolate",
         price : 100,
         thumbnail : "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/750/456/products/galletitas-pepitos1-997665b2ca5ad112c016255133287229-640-0.jpeg",
-        code : 15978643,
+        code : "dsf594as8",
         stock : 10
     },
     {
@@ -81,7 +81,7 @@ const productosArr = [
         descripcion : "probar q no se agrega si tiene el mismo code",
         price : 450,
         thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
-        code : 45789517,
+        code : "48aec566",
         stock : 15
     },
     {
@@ -89,7 +89,7 @@ const productosArr = [
         descripcion : "probar q no se agrega key diferente a los aceptados",
         price : 450,
         thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
-        code : 345345435347,
+        code : "34534as534a",
         stock : 15
     },
     {
@@ -97,7 +97,7 @@ const productosArr = [
         descripcion : "probar q no se agrega si tiene algun key-value sin completar",
         price : 450,
         thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
-        code : 487867867817,
+        code : "456123sdf8",
         stock : 15
     },
     {
@@ -105,7 +105,7 @@ const productosArr = [
         descripcion : "probar q no se agrega si tiene algun key-value sin completar",
         price : 450,
         thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
-        code : 489089098017,
+        code : "789fas54sg",
         stock : 15
     },
     {
@@ -113,7 +113,7 @@ const productosArr = [
         descripcion : "probar q no se agrega si tiene algun key-value sin completar",
         price : 450,
         thumbnail : "https://acdn.mitiendanube.com/stores/001/448/812/products/energy-ultra-mosnter1-641988d832c190fedf16276771317278-640-0.jpg",
-        code : 489089098017,
+        code : "a48d5e2666",
         stock : 15
     }
 ];
